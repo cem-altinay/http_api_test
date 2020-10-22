0a551a0355c1c2eb8a4b99d7edd519dc829278ba
cem-altinay <55686299+cem-altinay@users.noreply.github.com>
2020-10-22 23:16:43 +0300

    jsonplaceholder api

--- lib/data/data_api/post_api.dart
+++ lib/data/data_api/post_api.dart
@@ -1,1 +1,11 @@
+import 'package:http/http.dart' as http;
 
+class PostApi {
+  static Future getPosts() {
+    return http.get("https://jsonplaceholder.typicode.com/posts");
+  }
+
+  static Future getPost(int id) {
+    return http.get("https://jsonplaceholder.typicode.com/posts/$id");
+  }
+}
